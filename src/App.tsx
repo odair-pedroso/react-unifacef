ec5498bd9e9f861eaf2a3dd896c7529786a977e9
odair-pedroso <odairpedroso90@gmail.com>
2022-04-02 10:20:28 -0300

    botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import logo from './logo.svg';
+
 function App() {
+
+  const showMessage = () => {
+    alert('Teste');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Vamos aprender React
         </p>
+        <button onClick={showMessage}>Break the world</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Odair
+          React é legal
         </a>
       </header>
     </div>
