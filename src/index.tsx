c428b632338df60ea4cc68dad9c0979047f85a0f
odair-pedroso <odairpedroso90@gmail.com>
2022-04-02 16:38:49 -0300

    import axios api na index

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'mobx-react';
 import { router } from './mobx/';
 import { Router } from 'react-router-dom';
 import { syncHistoryWithStore } from 'mobx-react-router';
+import './apis/axios.api';
 import './plugins/sentry.plugin';
 
 const rootElement = document.getElementById('root');
