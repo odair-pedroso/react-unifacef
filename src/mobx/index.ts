fb0a7b560447af0d40db38d1139fd944ddb741f7
odair-pedroso <odairpedroso90@gmail.com>
2022-04-09 10:48:59 -0300

    endpoints e telas

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -1,9 +1,18 @@
 import { home } from '../containers/home/store';
 import { combustivel } from '../containers/combustivel/store';
 import { router } from './router.store';
+import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
+import { cache } from '../containers/cache/store';
+import eleicoes from '../containers/eleicoes';
+
 
 export {
   router,
   combustivel,
-  home
+  home,
+  starWars,
+  starWarsDetails,
+  cache,
+  eleicoes,
 }
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -1,15 +1,27 @@
-import Home from "../containers/home";
-import { RouteProps } from "react-router-dom";
-import Sobre from "../containers/sobre";
-import Combustivel from "../containers/combustivel";
+import { RouteProps } from 'react-router-dom';
+import StarWars from '../containers/star-wars';
+import Combustivel from '../containers/combustivel';
+import Home from '../containers/home';
+import Cache from '../containers/cache';
+import StarWarsDetails from '../containers/star-wars-details';
+import Tags from '../containers/tags';
+import Register from '../containers/register';
+import Eleicoes from '../containers/eleicoes';
 
 const publicUrl = process.env.PUBLIC_URL;
+
 interface EndPointsProps extends RouteProps {
   name?: string
 }
+
 export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/`, component: Home, exact: true },
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
+  { path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails },
+  { path: `${publicUrl}/star-wars/`, name: 'Star Wars', component: StarWars, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
-  { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
+  { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
+  { path: `${publicUrl}/tags`, name: 'Tags', component: Tags, exact: true },
+  { path: `${publicUrl}/register`, name: 'Register', component: Register, exact: true },
+  { path: `${publicUrl}/eleicoes`, name: 'Eleições', component: Eleicoes, exact: true },
 ];
